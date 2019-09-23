c2b14c5821a2cabddef0f0b19f1e79f5a23bb074
reytor <renier.tornes@gmail.com>
2019-09-23 13:38:07 -0400

    shop model

---
+++ models/Shop.js
@@ -0,0 +1,14 @@
+"use strict";
+module.exports = (sequelize, DataTypes) => {
+  const Shop = sequelize.define(
+    "Shop",
+    {
+      name: DataTypes.STRING
+    },
+    {}
+  );
+  Shop.associate = function(models) {
+    // associations can be defined here
+  };
+  return Shop;
+};
